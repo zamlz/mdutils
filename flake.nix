3609a57eff3e6a995351e88bd3ebbbc483349a5f
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2025-12-30 19:05:57 -0800

    added fmt checking and clippy to flake.nix build

--- flake.nix
+++ flake.nix
@@ -12,7 +12,20 @@
         naersk-lib = pkgs.callPackage naersk { };
       in
       {
-        packages.default = naersk-lib.buildPackage ./.;
+        packages.default = naersk-lib.buildPackage {
+          src = ./.;
+          doCheck = true;  # Run tests during build
+          nativeBuildInputs = with pkgs; [ rustfmt rustPackages.clippy python3 ];
+
+          # Run rustfmt and clippy checks before building
+          preBuild = ''
+            echo "Running rustfmt check..."
+            cargo fmt -- --check
+
+            echo "Running clippy..."
+            cargo clippy --all-targets --all-features -- -D warnings
+          '';
+        };
         devShells.default = with pkgs; mkShell {
           buildInputs = [
             cargo
