adb5f7b75cb2cb14235003d6c15d59ff6a06b524
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2026-01-26 10:07:59 -0800

    downgraded flakes for stability

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     naersk.url = "github:nix-community/naersk/master";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     utils.url = "github:numtide/flake-utils";
   };
 
